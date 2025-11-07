d31a2cd14046883e4c9ac1c2b74c0a8e163ed4df
Vai <144768865+viywolf@users.noreply.github.com>
2025-11-07 21:14:07 +1300

    wave's looking like a wave

--- scripts/wave.gd
+++ scripts/wave.gd
@@ -1,13 +1,10 @@
 extends Line2D
 
-@export var curveness : Vector2 = Vector2(-50,-50)
-@export var point_pos_array : Array[Vector2] = [
-	Vector2(100,200),
-	Vector2(200,100),
-	Vector2(300,200),
-	Vector2(400, 100),
-	Vector2(500, 200),
-	Vector2(600, 100),
+@export var point_pos_multiplier__x : int = 100
+@export var point_pos_multiplier__y : int = 100
+@export var number_of_points : int = 5
+@export var curveness_array : Array[Vector2] =[
+	Vector2(-50,50), Vector2(-50,-50)
 ]
 
 @onready var new_curve = Curve2D.new()
@@ -19,17 +16,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	draw_wave()
 
-func _input(event: InputEvent) -> void:
-	if(event.is_action_pressed("right")):
-		draw_wave()
-		
-
 func draw_wave() -> void:
 	points.clear()
 	clear_points()
 	new_curve.clear_points()
-	for point in point_pos_array:
-		new_curve.add_point(point, curveness)
+	for i in range(number_of_points):
+		var point_to_be_added : Vector2 = Vector2(
+			point_pos_multiplier__x * i + point_pos_multiplier__x, 
+			point_pos_multiplier__y * ((i+1) % 2) )
+		print(point_to_be_added)
+		new_curve.add_point(point_to_be_added, curveness_array[i%2])
 	var curved_points = new_curve.get_baked_points()
 	for point in curved_points:
 		add_point(point)
